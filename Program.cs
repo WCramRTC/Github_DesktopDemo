f897e0d185810fe18d7b2c25a552dea91e8522b0
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2023-09-22 21:32:50 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Github_DesktopDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Update One");
+        }
+    }
+}
